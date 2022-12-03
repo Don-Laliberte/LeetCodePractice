88e2cf5eec2f961728038adff6108728a4f62957
Donnietentoes <donhenrylaliberte@gmail.com>
2022-12-02 21:51:20 -0700

    Time: 12ms (43.31%), Space: 43.3 mb (80.91%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int romanToInt(String s) {
+        String[] numerals = s.split("");
+        int[] numberValues = new int[s.length()];
+        int finalNumber = 0;
+
+        for (int i = 0; i < numerals.length; i++) {
+            switch (numerals[i]) {
+                case "I" -> numberValues[i] = 1;
+                case "V" -> numberValues[i] = 5;
+                case "X" -> numberValues[i] = 10;
+                case "L" -> numberValues[i] = 50;
+                case "C" -> numberValues[i] = 100;
+                case "D" -> numberValues[i] = 500;
+                case "M" -> numberValues[i] = 1000;
+            }
+        }
+
+        for (int i = 0; i < numberValues.length; i++) {
+            if (i != numberValues.length - 1 && numberValues[i] < numberValues[i + 1]) {
+                finalNumber += numberValues[i + 1] - numberValues[i];
+                i++;
+            }
+            else {
+                finalNumber += numberValues[i];
+            }
+        }
+
+        return finalNumber;
+    }
+}
