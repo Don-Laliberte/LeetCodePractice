f13291ea91f2bc912ba0f494cf1de2af7e267af7
Donnietentoes <45013341+Don-Laliberte@users.noreply.github.com>
2023-07-08 02:13:12 -0600

    Time: 2 ms (99.46%), Space: 43.3 MB (54.07%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -10,55 +10,55 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode newListPointer = new ListNode(0);
-        ListNode newListStart = new ListNode(0, newListPointer);
-
+        ListNode newListStart = new ListNode();
+        ListNode newListPointer= newListStart;
+        int carryover = 0;
         while (l1 != null && l2 != null) {
-            if ((l1.val + l2.val + newListPointer.val) >= 10) {
-                newListPointer.val = l1.val + l2.val + newListPointer.val - 10;
-                newListPointer.next = new ListNode(1);
+            int sum = l1.val + l2.val + carryover;
+            if (sum > 9) {
+                carryover = 1;
+                sum=sum%10;
             }
             else {
-                newListPointer.val = l1.val + l2.val + newListPointer.val;
-                if (l1.next != null || l2.next != null) {
-                    newListPointer.next = new ListNode(0);
-                }
+                carryover = 0;
             }
-        l1 = l1.next;
-        l2 = l2.next;
-        newListPointer = newListPointer.next;
+            newListPointer.next = new ListNode(sum);
+            l1 = l1.next;
+            l2 = l2.next;
+            newListPointer = newListPointer.next;
         }
 
         while (l1 != null) {
-            System.out.println(l1.val + " " + newListPointer.val);
-            if ((l1.val + newListPointer.val) >= 10) {
-                newListPointer.val = l1.val + newListPointer.val - 10;
-                newListPointer.next = new ListNode(1);
+            int sum = l1.val + carryover;
+            if (sum > 9) {
+                carryover = 1;
+                sum=sum%10;
             }
             else {
-                newListPointer.val = l1.val + newListPointer.val;
-                    if (l1.next != null) {
-                        newListPointer.next = new ListNode(0);
-                    }
+                carryover = 0;
             }
+            newListPointer.next = new ListNode(sum);
             l1 = l1.next;
             newListPointer = newListPointer.next;
         }
 
         while (l2 != null) {
-            System.out.println(l2.val + " " + newListPointer.val);
-            if ((l2.val + newListPointer.val) >= 10) {
-                newListPointer.val = l2.val + newListPointer.val - 10;
-                newListPointer.next = new ListNode(1);
+            int sum = l2.val + carryover;
+            if (sum > 9) {
+                carryover = 1;
+                sum=sum%10;
             }
             else {
-                newListPointer.val = l2.val + newListPointer.val;
-                    if (l2.next != null) {
-                        newListPointer.next = new ListNode(0);
-                    }
+                carryover = 0;
             }
+            newListPointer.next = new ListNode(sum);
+
             l2 = l2.next;
             newListPointer = newListPointer.next;
+        }
+
+        if (carryover == 1) {
+                newListPointer.next = new ListNode(1);   
         }
 
         return newListStart.next;
