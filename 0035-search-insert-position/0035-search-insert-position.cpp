d42da92598ee372fd6ee020544d33fc7a367305f
Donnietentoes <45013341+Don-Laliberte@users.noreply.github.com>
2025-06-02 23:26:46 -0600

    Time: 0 ms (100%), Space: 13.8 MB (5.83%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        for (int i = 0; i < nums.size(); i++) {
-            if (target <= nums[i]) {
-                return i;
-            }
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] < target) left = mid + 1;
+            else right = mid - 1;
         }
-        return nums.size();
+        return left;
     }
 };
