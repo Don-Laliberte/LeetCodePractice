c5f829396487e4491fb621826246bef63ae5dac3
Donnietentoes <45013341+Don-Laliberte@users.noreply.github.com>
2025-09-15 08:20:17 -0600

    Time: 0 ms (100%), Space: 8.6 MB (18.73%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> memo(n + 1, -1);
+        return climb(n, memo);
+    }
+
+    int climb(int n, vector<int>& memo) {
+        if (n <= 2) {
+            return n;
+        }
+
+        if (memo[n] != -1) {
+            return memo[n];
+        }
+
+        memo[n] = (climb(n-1, memo)) + (climb(n-2, memo));
+        return memo[n];
+    }
+};
