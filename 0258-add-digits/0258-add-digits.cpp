9559376c1b734d4dd977f65091c360473906a180
Donnietentoes <45013341+Don-Laliberte@users.noreply.github.com>
2025-09-23 23:07:31 -0600

    Time: 0 ms (100%), Space: 8.8 MB (9.58%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+
+        }
+        if (sum <= 9 && sum >= 0) {
+            return sum;
+        }
+        else {
+            return addDigits(sum);
+        }
+    }
+};
