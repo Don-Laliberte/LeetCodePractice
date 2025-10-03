74a0300f396fe2151fc0ed6b32af2960385fa156
Donnietentoes <45013341+Don-Laliberte@users.noreply.github.com>
2025-10-02 19:19:12 -0600

    Time: 0 ms (100%), Space: 9.8 MB (48.69%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,19 +3,18 @@ public:
     bool isAnagram(string s, string t) {
         if (s.size() != t.size()) return false;
 
-        // charcount<char, int> if this contained unicode characters
-        vector <int> charcount(26,0); //27 letters indexed from 0
+        vector<int> charcount(26,0);
 
         for (char c : s) {
-            charcount[c - 'a']++; //a in ASCII is 96 so we subtract to get our dynamic array index value
+            charcount[c - 'a']++; 
         }
 
         for (char c : t) {
-            charcount[c - 'a']--; // Subtracts all counts from the second string
+            charcount[c - 'a']--; 
         }
 
         for (int num : charcount) {
-            if (num != 0) return false; // Check if there's a different amount of characters
+            if (num != 0) return false; 
         }
 
         return true;
