4491c9145315cff84e79f83aa4ff4d0fa4dd0fca
Donnietentoes <45013341+Don-Laliberte@users.noreply.github.com>
2025-10-14 22:26:48 -0600

    Time: 3 ms (59.41%), Space: 8.9 MB (45.46%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -2,9 +2,10 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if (n > 0 && ((int)pow(3,19) % n) == 0.0) {
-            return true;
+        if (n <= 0) return false;
+        while (n % 3 == 0) {
+            n /= 3;
         }
-        return false;
+        return n == 1;
     }
 };
